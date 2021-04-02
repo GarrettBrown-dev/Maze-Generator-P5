4dd6f8ca187d14920149ea09b11b6a7ee623c88b
Garrett <gman9mm@live.com>
2021-04-02 10:47:28 -0700

    Add if statements for each wall.

--- sketch.js
+++ sketch.js
@@ -25,15 +25,24 @@ function draw() {
 function Cell(i, j) {
   this.i = i; //column #
   this.j = j; // row #
+  this.walls = [true, true, true, true]; //top, right, bottom, left.
 
   this.show = function () {
     var x = this.i * w;
     var y = this.j * w;
     stroke(255);
-    line(x, y, x + w, y);
-    line(x + w, y, x + w, y + w);
-    line(x + w, y + w, x, y + w);
-    line(x, y + w, x, y);
+    if (this.walls[0]) {
+      line(x, y, x + w, y);
+    }
+    if (this.walls[1]) {
+      line(x + w, y, x + w, y + w);
+    }
+    if (this.walls[2]) {
+      line(x + w, y + w, x, y + w);
+    }
+    if (this.walls[3]) {
+      line(x, y + w, x, y);
+    }
     // noFill();
     // rect(x, y, w, w);
   };
